4ee448611915f9a9278fe8687fc6fdcdb5820756
Timothy MacNary <macnary17@up.edu>
2018-03-02 14:02:33 -0800

    cleaning up TODOS

--- ASP/UP_EHR/Controllers/HomeController.cs
+++ ASP/UP_EHR/Controllers/HomeController.cs
@@ -87,12 +87,7 @@ namespace UP_EHR.Controllers
 
             db_logic.PostSummaryInputData(model);
 
-            //TODO: tmacnary, this puts the correct data into the correct row, but the model doesn't have the right values besides that,
-            //so upon completion, all other data is lost until reloading summary page. Figure out how to call httpget summary here with providing
-            //the correct DB_ID.
             return RedirectToAction("Summary", new { databaseId = db_id});
-
-            //return View(model); 
         }
 
         [HttpPost]
